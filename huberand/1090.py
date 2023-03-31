2c8bd749a06070c5a0594d8d050bfceffde5607b
Andreas Huber <andreas.huber@intel.com>
2023-03-31 01:53:15 -0700

    backup commit

--- huberand/1090.py
+++ huberand/1090.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import metrics
 
+from sklearn import 
+
 x_train, y_train, x_test, y_test = (
     np.load("./1090/x_train.npy"),
     np.load("./1090/y_train.npy"),
@@ -18,34 +20,34 @@ from sklearn.ensemble import RandomForestRegressor as RFStock
 
 from daal4py.sklearn.ensemble import RandomForestRegressor as RFOptimized
 
-params = {
-    "n_estimators": 150,
-    "random_state": 44,
-    "n_jobs": -1,
-}
+# params = {
+#     "n_estimators": 150,
+#     "random_state": 44,
+#     "n_jobs": -1,
+# }
 
-start = perf_counter()
-rf = RFOptimized(**params).fit(x_train, y_train)
-train_patched = perf_counter() - start
-print(f"Intel® extension for Scikit-learn time: {train_patched:.2f} s")
-y_pred_opt = rf.predict(x_test)
-mse_opt = metrics.mean_squared_error(y_test, y_pred_opt)
-print(f"Intel® extension for Scikit-learn Mean Squared Error: {mse_opt}")
+# start = perf_counter()
+# rf = RFOptimized(**params).fit(x_train, y_train)
+# train_patched = perf_counter() - start
+# print(f"Intel® extension for Scikit-learn time: {train_patched:.2f} s")
+# y_pred_opt = rf.predict(x_test)
+# mse_opt = metrics.mean_squared_error(y_test, y_pred_opt)
+# print(f"Intel® extension for Scikit-learn Mean Squared Error: {mse_opt}")
 
-start = perf_counter()
-rf = RFStock(**params).fit(x_train, y_train)
-train_patched = perf_counter() - start
-print(f"stock-learn time: {train_patched:.2f} s")
-y_pred = rf.predict(x_test)
-mse_opt = metrics.mean_squared_error(y_test, y_pred)
-print(f"stock-learn Mean Squared Error: {mse_opt}")
+# start = perf_counter()
+# rf = RFStock(**params).fit(x_train, y_train)
+# train_patched = perf_counter() - start
+# print(f"stock-learn time: {train_patched:.2f} s")
+# y_pred = rf.predict(x_test)
+# mse_opt = metrics.mean_squared_error(y_test, y_pred)
+# print(f"stock-learn Mean Squared Error: {mse_opt}")
 
-fig, ax = plt.subplots()
+# fig, ax = plt.subplots()
 
-ax.scatter(y_test, y_test, label="ground truth", s=2, c="black")
-ax.scatter(y_test, y_pred_opt, label="sklex", s=2, c="g", alpha=0.2)
-ax.scatter(y_test, y_pred, label="stock", s=2, c="r", alpha=0.2)
+# ax.scatter(y_test, y_test, label="ground truth", s=2, c="black")
+# ax.scatter(y_test, y_pred_opt, label="sklex", s=2, c="g", alpha=0.2)
+# ax.scatter(y_test, y_pred, label="stock", s=2, c="r", alpha=0.2)
 
-ax.legend()
+# ax.legend()
 
-plt.savefig("1090/y_test.png")
+# plt.savefig("1090/y_test.png")
